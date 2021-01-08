b81060d8aa1abb1635c4d20f5efabfaf0ad7fdad
BYaf <9634207@qq.com>
2021-01-08 20:24:55 +0800

    添加websocket测试

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"game_frame/net"
+	"golang.org/x/net/websocket"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 	"time"
 )
 
-func main() {
+
+//func main() {
 	//var a  = 2048
 	////for {
 	//	c := string(a)
@@ -26,23 +33,85 @@ func main() {
 		//a++
 	//}
 	//
-	reqChan := make(chan *net.Message)
-	resChan := make(chan *net.Message)
-	go client(resChan)
-	tcp := net.NewTcpListen("192.168.3.156:30000",reqChan,resChan)
-	go func() {
-		for  {
-			v := <- reqChan
-			fmt.Println(v)
-			//v.Code = v.Code + 10
-			//v.Body = []byte(fmt.Sprint("回复消息"))
-			//resChan <- v
+	//reqChan := make(chan *net.Message)
+	//resChan := make(chan *net.Message)
+	//go client(resChan)
+	//tcp := net.NewTcpListen("192.168.3.156:30000",reqChan,resChan)
+	//go func() {
+	//	for  {
+	//		v := <- reqChan
+	//		fmt.Println(v)
+	//		//v.Code = v.Code + 10
+	//		//v.Body = []byte(fmt.Sprint("回复消息"))
+	//		//resChan <- v
+	//	}
+	//
+	//}()
+	//defer tcp.Close()
+	//net.SocketListenEvent(tcp, net.Handle)
+//}
+
+var i = 0
+func upper(ws *websocket.Conn) {
+	var err error
+	i++
+	var cnt = i
+	connectMap[fmt.Sprint(i)] = ws
+
+	for {
+		var reply string
+		if len(connectMap) > 1{
+			websocket.Message.Send(connectMap["1"], strings.ToUpper(fmt.Sprint(len(connectMap))))
+		}
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println(err)
+			if err == io.EOF{
+				fmt.Println(string(cnt),"下线了")
+				delete(connectMap,string(cnt))
+			}
+			break
 		}
 
-	}()
-	defer tcp.Close()
-	net.SocketListenEvent(tcp, net.Handle)
+		if err = websocket.Message.Send(ws, strings.ToUpper(reply+"123")); err != nil {
+			fmt.Println(err)
+		}else {
+			websocket.Message.Send(ws, strings.ToUpper("12312312"))
+		}
+
+	}
 }
+
+var connectMap = make(map[string]*websocket.Conn )
+
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		return
+	}
+
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/upper", websocket.Handler(upper))
+	http.HandleFunc("/", index)
+
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	//router := mux.NewRouter()
+	//router.HandleFunc("/ws", Myws)
+	//if err := http.ListenAndServe("127.0.0.1:8080", router); err != nil {
+	//	fmt.Println("err:", err)
+	//}
+	//go h.run()
+
+}
+
+
 
 func client(data chan *net.Message)  {
 	time.Sleep(time.Second *3)
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module game_frame
 
 go 1.15
+
+require (
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
+)
