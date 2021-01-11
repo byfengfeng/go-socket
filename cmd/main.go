3c03ff8eddd7419f77129832f21f98f97d9c716e
BYaf <9634207@qq.com>
2021-01-11 20:00:35 +0800

    更新socket测试方法

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"game_frame/net"
+	pb "game_frame/probuf"
+	"github.com/gogo/protobuf/proto"
 	"golang.org/x/net/websocket"
 	"html/template"
 	"io"
@@ -14,7 +15,7 @@ import (
 )
 
 
-//func main() {
+func main() {
 	//var a  = 2048
 	////for {
 	//	c := string(a)
@@ -30,17 +31,25 @@ import (
 	//		fmt.Println(len(bytes),"----",a)
 	//		//break
 	//	}
-		//a++
+	//	a++
 	//}
 	//
+
+	//TCP测试
 	//reqChan := make(chan *net.Message)
 	//resChan := make(chan *net.Message)
 	//go client(resChan)
 	//tcp := net.NewTcpListen("192.168.3.156:30000",reqChan,resChan)
-	//go func() {
+	//go func(){
 	//	for  {
 	//		v := <- reqChan
+	//
 	//		fmt.Println(v)
+	//		notifi := &pb.ResMailChange{
+	//		}
+	//		proto.Unmarshal(v.Body,notifi)
+	//		fmt.Println(notifi.MailId)
+	//		fmt.Println(notifi.MailChangeType)
 	//		//v.Code = v.Code + 10
 	//		//v.Body = []byte(fmt.Sprint("回复消息"))
 	//		//resChan <- v
@@ -49,7 +58,16 @@ import (
 	//}()
 	//defer tcp.Close()
 	//net.SocketListenEvent(tcp, net.Handle)
-//}
+
+	//WEBSOCKET测试
+	http.Handle("/upper", websocket.Handler(upper))
+	http.HandleFunc("/", index)
+
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 var i = 0
 func upper(ws *websocket.Conn) {
@@ -67,7 +85,7 @@ func upper(ws *websocket.Conn) {
 			fmt.Println(err)
 			if err == io.EOF{
 				fmt.Println(string(cnt),"下线了")
-				delete(connectMap,string(cnt))
+				delete(connectMap,fmt.Sprint(cnt-1))
 			}
 			break
 		}
@@ -93,14 +111,37 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
-func main() {
-	http.Handle("/upper", websocket.Handler(upper))
-	http.HandleFunc("/", index)
+func getbys(data interface{}) ([]byte, error) {
+	return proto.Marshal(data.(proto.Message))
+}
 
-	if err := http.ListenAndServe(":9999", nil); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+//func main() {
+	//code := 13001
+	//notifi := &pb.ResMailChange{
+	//	MailChangeType: int32(3),
+	//	MailId: []int64{23001},
+	//}
+	////bytes, err := getbys(notifi)
+	////if err != nil{
+	////	panic(err)
+	////}
+	////size := notifi.XXX_Size()
+	////a, err := notifi.Marshal()
+	//marshal, _ := proto.Marshal(notifi)
+	//bytes, _ := json.Marshal(notifi)
+	////notifi.ProtoMessage()
+	////bytes, ints := notifi.Descriptor()
+	//fmt.Println(len(marshal))
+	//fmt.Println(len(bytes))
+	//fmt.Println(len(bytes))
+
+	//http.Handle("/upper", websocket.Handler(upper))
+	//http.HandleFunc("/", index)
+	//
+	//if err := http.ListenAndServe(":9999", nil); err != nil {
+	//	fmt.Println(err)
+	//	os.Exit(1)
+	//}
 
 	//router := mux.NewRouter()
 	//router.HandleFunc("/ws", Myws)
@@ -109,20 +150,24 @@ func main() {
 	//}
 	//go h.run()
 
-}
+//}
 
 
 
 func client(data chan *net.Message)  {
 	time.Sleep(time.Second *3)
 	tcpDial := net.NewTcpDial("192.168.3.156:30000")
-
-	users := "1"
-	for j:=2; j < 50; j++{
-		users+=fmt.Sprint(j)
-	}
+	notifi := &pb.ResMailChange{
+			MailChangeType: int32(3),
+			MailId: []int64{23001},
+		}
+	marshal, _ := proto.Marshal(notifi)
+	//users := "1"
+	//for j:=2; j < 50; j++{
+	//	users+=fmt.Sprint(j)
+	//}
 	for i:=1; i <= 10; i++ {
-		marshal, _ := json.Marshal(users)
+		//marshal, _ := json.Marshal(users)
 		data <- &net.Message{
 			Code: int32(1000+i),
 			Connect: tcpDial,
--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module game_frame
 go 1.15
 
 require (
+	github.com/gogo/protobuf v1.3.1
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
+	google.golang.org/protobuf v1.25.0
 )
