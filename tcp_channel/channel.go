4cdcceced3453e3fcc4424dffc6d67ac97a08ab7
BYaf <9634207@qq.com>
2021-08-03 16:16:26 +0800

    更新tcp协议读取

--- tcp_channel/channel.go
+++ tcp_channel/channel.go
@@ -1,6 +1,9 @@
 package tcp_channel
 
-import "net"
+import (
+	"errors"
+	"net"
+)
 
 type channel struct {
 	conn net.Conn
@@ -13,7 +16,31 @@ func (c *channel) write(bytes []byte) error {
 	return err
 }
 
-func (c *channel) read() error {
+func (c *channel) read() (err error) {
+	var(
+		headByte []byte
+		length uint16
+	)
+	for  {
+		headByte = make([]byte,2)
+		_, err = c.conn.Read(headByte)
+		if err != nil {
+			return err
+		}
+		length = uint16(headByte[0]) << 8 | uint16(headByte[1])
 
+		if length <= 0{
+			return errors.New("错误消息长度")
+		}
+		body := make([]byte,length-2)
+		_, err = c.conn.Read(body)
+		if err != nil {
+			return err
+		}
+		c.readChan <- body
+	}
+	if err != nil {
+		return
+	}
 	return nil
 }
